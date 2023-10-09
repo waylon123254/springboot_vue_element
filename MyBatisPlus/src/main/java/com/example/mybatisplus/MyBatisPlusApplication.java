6988e27aeabd32e1257f743fa92df971be8e55ca
LENOVO <lv2192327406@outlook.com>
2023-10-09 12:02:40 +0800

    myBaitsPlus

---
+++ MyBatisPlus/src/main/java/com/example/mybatisplus/MyBatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisPlusApplication.class, args);
+    }
+
+}
